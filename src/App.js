7994044db23f118ef58c116a9e10727ba7dea312
Kat <katrinaburwash_17@hotmail.com>
2023-02-24 14:27:25 +0100

    finished game and installed gh npm

--- src/App.js
+++ src/App.js
@@ -2,20 +2,45 @@ import './App.css';
 import Die from './Die';
 import React from "react";
 import { nanoid } from 'nanoid';
-// import {useWindowSize} from 'react-use';
-// import Confetti from 'react-confetti';
+import {useWindowSize} from 'react-use';
+import Confetti from 'react-confetti';
 
 function App() {
-  // const { width, height } = useWindowSize()
+  const { width, height } = useWindowSize()
   const [dice, setDice] = React.useState(generateRandomDice())
-
-  function rollDice(){
-    setDice(generateRandomDice())
-  }
+  const [tenzies, setTenzies] = React.useState(false)
+  const [count, setCount] = React.useState(0)
 
   const diceElements = dice.map(die => {
-    return <Die key={die.id} value={die.value}/>
+    return <Die key={die.id} value={die.value} isHeld={die.isHeld} freezeDie={()=>holdDice(die.id)}/>
   })
+
+  React.useEffect(()=>{
+    const allDiceHeld = dice.every(die => die.isHeld)
+    const dieNumber = dice[0].value
+    const allSameValues = dice.every(die => die.value === dieNumber)
+
+    if(allDiceHeld && allSameValues){
+      setTenzies(true)
+    }
+  },[dice])
+
+  function rollDice(){
+    setCount(prevState => prevState + 1)
+    setDice(oldDice => oldDice.map(die => {
+      const randomNumber = Math.floor(Math.random()*6)+1
+      if(die.isHeld){
+        return die
+      }else {
+        return {
+          id: nanoid(),
+          value:randomNumber,
+          isHeld: false
+        }
+        }
+    }))
+    
+  }
 
   function generateRandomDice(){
     const diceArray = []
@@ -24,27 +49,57 @@ function App() {
       diceArray.push({
         id: nanoid(),
         value:randomNumber,
-        isHeld:false
+        isHeld: false
       })
     }
     return diceArray
   }
 
+  function holdDice(id){
+   setDice(oldDice => oldDice.map(die => {
+    if(id === die.id){
+      return {...die, isHeld:!die.isHeld}
+    }else{
+      return die
+    }
+   }))
+  }
+
+  function playGame(){
+    if(tenzies){
+      setTenzies(false)
+      setDice(generateRandomDice())
+      setCount(0)
+    }else{
+      rollDice()
+    }
+  }
 
   return (
     <div>
-      {/* <Confetti  width={width} height={height} /> */}
+      {tenzies && <Confetti  width={width} height={height} />}
         <header>
           <h1>Tenzies</h1>
         </header>
 
-      <main>
-        <div className="diceContainer">
-          {diceElements}
+      
+        <div className="instructions">
+          <h4>
+            Roll until all dice are the same. Click each die to freeze it at it's current value between rolls.
+          </h4>
         </div>
 
-        <button className="rollBtn" onClick={rollDice}>Roll</button>
-      </main>
+        <main>
+          {tenzies? 
+          <div><h1>You won with {count} rolls!</h1></div> 
+          : <div><h1>Count: {count}</h1></div>}
+
+          <div className="diceContainer">
+            {diceElements}
+          </div>
+
+          <button className="rollBtn" onClick={playGame}>{tenzies?"New Game": "Roll"}</button>
+        </main>
     </div>
   )
     
--- src/Die.js
+++ src/Die.js
@@ -1,8 +1,11 @@
 import React from "react"
 
 export default function Die(props){
+    const dieStyle = {
+        backgroundColor: props.isHeld? "#4B77BE" : ""
+    }
     return (
-        <div className="dieFace">
+        <div className="dieFace" style={dieStyle} onClick={props.freezeDie}>
             {props.value}
         </div>
     )
