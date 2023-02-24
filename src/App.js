056efba4ffb29a44d28d3cc6fc57fa8ba9a0e6ad
Kat <katrinaburwash_17@hotmail.com>
2023-02-24 12:03:04 +0100

    css styling and generated random dice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Die from './Die';
+import React from "react";
+import { nanoid } from 'nanoid';
+// import {useWindowSize} from 'react-use';
+// import Confetti from 'react-confetti';
 
 function App() {
+  // const { width, height } = useWindowSize()
+  const [dice, setDice] = React.useState(generateRandomDice())
+
+  function rollDice(){
+    setDice(generateRandomDice())
+  }
+
+  const diceElements = dice.map(die => {
+    return <Die key={die.id} value={die.value}/>
+  })
+
+  function generateRandomDice(){
+    const diceArray = []
+    for(let i=0; i < 10; i++){
+      const randomNumber = Math.floor(Math.random()*6)+1
+      diceArray.push({
+        id: nanoid(),
+        value:randomNumber,
+        isHeld:false
+      })
+    }
+    return diceArray
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Confetti  width={width} height={height} /> */}
+        <header>
+          <h1>Tenzies</h1>
+        </header>
+
+      <main>
+        <div className="diceContainer">
+          {diceElements}
+        </div>
+
+        <button className="rollBtn" onClick={rollDice}>Roll</button>
+      </main>
     </div>
-  );
+  )
+    
 }
 
 export default App;
